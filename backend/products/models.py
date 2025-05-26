2f0d73fb38e4f9d8b402095c62f8f4fe99116c83
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-05-26 23:46:26 +0330

    datatypes

--- backend/products/models.py
+++ backend/products/models.py
@@ -3,13 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='ths is cool!') 
-    
+
+    title = models.CharField(max_length=120)  
+    description = models.TextField(blank=True, null=False , default='default description')
+    price = models.DecimalField(decimal_places=2 , max_digits=10 )
+    summary = models.TextField()
 
     
-
-
-
