1dd253ad4ede1e3a57b453fd219bd4054586cb32
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-05-30 21:57:28 +0330

    condition for template value

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,30 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
 def home_view( request, *args , **kwags):
     
     print(f"the request is : {request}")
     print(f"args is : {args} , kwags is : {kwags}")
     print(f"the user is : {request.user}")
-   
-    
 
     test_template = {
-        'inner_dict1': 'this is the inner one' , 
+        'inner_dict1': 2024  , 
         'inner_dict2': 'this is the inner two' , 
         'inner_dict3': 'this is the inner three' , 
+        'my_html' : "<h1>Hello something</h1>" , 
         'inner_dict4': 'this is the inner four' ,
     }
-    
+
     my_context = {
         'my_text' : "this is about me" ,
         'my_number' : 123 , 
         'my_list' : [12,123,123123] ,
-        'inner'  : test_template
+        'inner'  : test_template , 
+        'this_is_true' : True , 
     }
-    
 
-    return render(request , "home.html" ,  my_context) # django rendering the html doc
+    return render(request , "home.html" ,  my_context) # django rendering the html doc as a thing that django deos
 
 
